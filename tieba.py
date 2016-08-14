0206a988090066ee6e829025a77f4eac06f0e827
Lucky_qin <wenningqin@163.com>
2016-08-14 17:21:56 +0800

    删除注释

--- tieba.py
+++ tieba.py
@@ -82,8 +82,6 @@ class  UrlParser():
         r_user_3 = re.compile(r'class="d_post_content j_d_post_content ">(.*?)</div>')
         user_list_3 = r_user_3.findall(html_content.decode('utf-8'))
         #time
-        #<span class="tail-info">30楼</span><span class="tail-info">2016-08-08 21:24</span>
-        #<span class="tail-info">29楼</span><span class="tail-info">2016-08-08 21:23</span>
         #<span class="tail-info">11楼</span><span class="tail-info">2016-08-08 20:50</span>
         r_user_4 = re.compile(r'<span class="tail-info">.*<span class="tail-info">(.*?)</span></div>')
         user_list_4 = r_user_4.findall(html_content.decode('utf-8'))
@@ -108,53 +106,6 @@ class  UrlParser():
         else:
             print("len not match")
 
-        '''
-        prog = re.compile('<div class="d_author">(.*?)</div><br>            </cc>        <br>        <div')
-        context_list = prog.findall(html_content.decode('utf-8'))
-
-        user_url = ''
-        user_name = ''
-        user_star = ''
-        user_level = 0
-        user_context = ''
-        user_id = 0
-        user_time = ''
-        print('context_list len', len(context_list))
-        for i in range(len(context_list)):
-            single = context_list[i]
-            match_obj = re.match( r'(.*) <li class="icon"> (.*?) href="(.*?)"><img username="(.*?)" class(.*)', single, re.M|re.I)
-            if match_obj:
-                user_url = mainUrl + match_obj.group(3)
-                user_name = match_obj.group(4)
-            else:
-                continue
-                print("no match user url and name")
-
-            match_obj = re.match( r'(.*) class="j_icon_slot"  title="(.*?)" class(.*) <div class="d_badge_lv">(.*?)</div>(.*)', single, re.M|re.I)
-            if match_obj:
-                user_star = match_obj.group(2)
-                user_level = match_obj.group(4)
-            else:
-                continue
-                print('no match user star and level')
-
-            if not user_list[user_name]:
-                user_id = db.UpdateUserInfo(user_name, user_url, user_star, user_level)
-                user_list[user_name] = (user_id, user_url)
-            else:
-                user_id = user_list[user_name][0]
-
-            match_obj = re.match( r'(.*) class="d_post_content j_d_post_content ">(.*?)</div>(.*)<span class="tail-info">(.*?)</span></div>(.*)', single, re.M|re.I)
-            if match_obj:
-                user_context = matchObj.group(2)
-                user_time = match_obj.group(4)
-            else:
-                continue
-                print('no match user context')
-
-            context_sql = "insert into {0}(user_id, context, cur_time) Value({1},'{2}','{3}')".format(self.table_name, user_id, user_context, user_time)
-            db.Execute(context_sql)
-        '''
         self.GetUserInfo()
 
         return True
@@ -175,37 +126,6 @@ class  UrlParser():
                 r_context = re.compile('<ul class="new_list clearfix">(.*?)</ul>')
                 context_list = r_context.findall(html_content.decode('GBK'))
 
-                '''
-                for i in range(len(context_list)):
-                    obj = re.match( r'(.*)<div class="n_post_time">(.*)</div><div class="n_type type_(.*)"></div>(.*)', context_list[i], re.M|re.I)
-
-                    tmp_time, tmp_type, tmp_context, tmp_url, tmp_name, tmp_titil = '', '', '', '', '', ''
-                    if obj:
-                        tmp_time = obj.group(2)
-                        tmp_type = obj.group(3)
-                    else:
-                        print('getuserinfo error, time or type fail, ' + i)
-                        continue
-                    
-                    if tmp_type == 'huifu':
-                        con_obj = re.compile(r'(.*)<div class="n_contain"><div><div class="thread_name"><a href="(.*)" class="reply_content" target="_blank" locate="thread_replayer#ihome_v1">(.*)</a></div></div><div class="n_txt_huifu"><div class="n_triangle"></div><a href="(.*)" target="_blank" class="titletxt" title="(.*)">(.*)</a><span class="userinfo_split"></span><a href="(.*)" target="_blank" class="n_name" title="(.*)">(.*)吧</a></div></div>(.*)')
-                        tmp_url = mainUrl + con_obj.group(2)
-                        tmp_context = con_obj.group(3)
-                        tmp_titil = con_obj.group(5)
-                        tmp_name = con_obj.group(9)
-                    elif tmp_type == 'zhuti':
-                        con_obj = re.compile(r'(.*)<div class="n_contain"><div><div class="thread_name"><a href="(.*)" target="_blank" class="title" locate="thread_author#ihome_v1" title="(.*)">(.*)</a><span class="userinfo_split"></span><a href="(.*)" target="_blank" class="n_name" title="(.*)">(.*)吧</a></div></div><div class="n_txt">(.*)</div></div>(.*)')
-                        tmp_url = mainUrl + con_obj.group(2)
-                        tmp_context = con_obj.group(8)
-                        tmp_titil = con_obj.group(3)
-                        tmp_name = con_obj.group(7)
-                    else:
-                        print('type recognition ,' + tmp_type)
-                        continue
-
-                    tmp_sql = "insert into user_dynamics(userid, tieba_name, context, url, titil, create_time, update_time) Value({0}, '{1}', '{2}', '{3}', '{4}', '{5}', now())".format(v[0], tmp_name, tmp_context, tmp_url, tmp_titil, tmp_time)
-                    db.Execute(tmp_sql)
-                '''
         print('GetUserInfo End')
 
     def SetUrl(self, url):
